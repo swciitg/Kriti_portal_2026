222609f90b7b93c3fccc87ffb0012cf6e371ec18
vedantShah30 <pravincj27@gmail.com>
2025-12-11 01:04:30 +0530

    registerTeam

---
+++ backend/src/controllers/techSecy/registerTeam.js
@@ -0,0 +1,58 @@
+import TechSecy from "../../model/techSecy.js";
+import Team from "../../model/team.js";
+import PS from "../../model/ps.js";
+
+export const registerTeam = async (req, res) => {
+  try {
+    const { techSecyId, psId, teamMembers } = req.body;
+    const techSecy = await TechSecy.findById(techSecyId);
+    if (!techSecy) {
+      return res.send(404).json({ message: "tech secy not found" });
+    }
+    const ps = await PS.findById(psId);
+    if (!ps) {
+      return res.send(404).json({ message: "ps not found" });
+    }
+    const existingTeam = await Team.findOne({
+      techSecy: techSecyId,
+      ps: psId,
+    });
+    if (existingTeam) {
+      return res.send(400).json({ message: "team already registered" });
+    }
+    if (teamMembers.length > ps.teamStrength) {
+      return res
+        .send(400)
+        .json({
+          message: `this ps allows maximum of ${ps.teamStrength} people`,
+        });
+    }
+    const emailRegex = /.+\@.+\..+/;
+    for (let i = 0; i < teamMembers.lenth; i++) {
+      const m = teamMembers[i];
+      if (!m.name || !m.email || !m.rollNumber) {
+        return res
+          .send(400)
+          .json({ message: "required fields for team members are missing" });
+      }
+      if (!emailRegex.test(m.email)) {
+        return res
+          .send(400)
+          .json({ message: `invalid email format for team member ${m.name}` });
+      }
+    }
+    const team = new Team({
+      techSecy: techSecy._id,
+      hostelId: techSecy.hostelId,
+      ps: ps._id,
+      teamMembers,
+    });
+    await team.save();
+    return res
+      .status(201)
+      .json({ message: "Team registered successfully", team });
+  } catch (error) {
+    console.error(err);
+    return res.send(500).json({ message: "internal sever error" });
+  }
+};
