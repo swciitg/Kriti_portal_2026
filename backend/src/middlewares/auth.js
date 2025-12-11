9837dc0b5bbb0c4accd388ccd40772f39a2f00d8
vedantShah30 <pravincj27@gmail.com>
2025-12-12 00:16:37 +0530

    fixed the wrong merge conflict

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -132,22 +132,6 @@ export function handleRouteAccess(req, res, next) {
       }
     }
 
-    if (role === "TechSecy") {
-      const allowed = [
-        
-      ];
-      const startsWithAllowed = ["/api/v1/techsecy/register-team/"];
-      if (
-        !allowed.includes(route) &&
-        !startsWithAllowed.some((p) => route.startsWith(p))
-      ) {
-        return res.status(403).json({
-          success: false,
-          message: "Forbidden to access this route",
-        });
-      }
-    }
-
     // will do similar for other roles as well
 
     next();
