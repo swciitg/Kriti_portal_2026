b643a90c7b20215555b20938c462697634437f6a
vedantShah30 <pravincj27@gmail.com>
2025-12-16 15:00:00 +0530

    new request model

---
+++ backend/src/model/request.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema({
+  from: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  to: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  psId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PS",
+    required: true,
+  },
+  requestType: {
+    type: String,
+    enum: ["EDIT_TEAM"], // update this later to have different requests
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending",
+  },
+});
+
+export default mongoose.model("Request", requestSchema);
