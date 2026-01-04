36f54f3c4de70ff23d0bb4e668324b136eee60fd
srinjoy-zeron <srinjoy@zeron.one>
2026-01-04 13:53:17 +0530

    time bug fixed in techsecy/problem-statement

--- backend/src/controllers/ps/protected/getPSProtected.js
+++ backend/src/controllers/ps/protected/getPSProtected.js
@@ -6,17 +6,18 @@ export async function getPSProtected(req, res) {
         const role = req.user?.role
     
         if(role && role === "TechSecy") {
-            psList = await PS.find({
-                                startDate : {
-                                    $lte :  new Date()
-                                            .toLocaleString("en-IN", {
-                                                timeZone: "Asia/Kolkata"
-                                            })
-                                }
-                            }).select("name startDate prep midEvalExist pdf points teamStrength ")
+            psList = await PS.find().
+                            select("name startDate prep midEvalExist pdf points teamStrength ")
             
         }
-    
+
+        psList = psList.filter(ps => {
+            const curr_date = new Date()
+            const ps_start_date = new Date(ps.startDate)
+            // console.log(curr_date , ps_start_date)
+            return curr_date > ps_start_date;
+        })
+
         return res.status(200).json({
             success : true ,
             ps : psList
