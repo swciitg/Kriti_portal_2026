8f64487e570d4b8cf5e50603b9ad9ecda34daf76
unknown <ashishmoreee123@gmail.com>
2026-01-05 00:55:56 +0530

    prod View team issue fix 3

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -207,7 +207,7 @@ export function handleRouteAccess(req, res, next) {
       "/v1/pssubmission/submit",
       "/v1/pssubmission/view/",
       "/v1/pssubmission/upload-temp",
-      "/v1/ps/protected"
+      "/v1/ps",
     ];
     if (
       !allowed.includes(route) &&
