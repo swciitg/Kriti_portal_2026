63eff804abbc8d6ee0c998bb7a78ba65a36856fd
unknown <ashishmoreee123@gmail.com>
2026-01-05 00:53:26 +0530

    prod View team issue fix 2

--- backend/src/controllers/ps/public/getPS.js
+++ backend/src/controllers/ps/public/getPS.js
@@ -4,9 +4,6 @@ import Team from "../../../model/team.js";
 
 export const getPS = async (req, res) => {
   try {
-    console.log("Fetching started PS");
-    console.log("User:", req.user);
-
     const startedPS = await PS.find({
       startDate: { $lte: new Date() }
     }).select("_id name startDate prep midEvalExist pdf points teamStrength registrationDeadline");
@@ -19,9 +16,6 @@ export const getPS = async (req, res) => {
       const techSecyData = await TechSecy.findOne({ user: req.user._id });
       if (techSecyData) {
         hostelId = techSecyData.hostelId;
-        console.log("TechSecy found with hostelId:", hostelId);
-      } else {
-        console.log("TechSecy not found for user:", req.user._id);
       }
     } 
 
@@ -36,7 +30,6 @@ export const getPS = async (req, res) => {
           hostelId: hostelId
         });
         teamRegistered = !!teamExists;
-        console.log(`PS: ${ps.name}, hostelId: ${hostelId}, teamExists: ${teamRegistered}`);
       }
 
       const psObj = ps.toObject ? ps.toObject() : JSON.parse(JSON.stringify(ps));
--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -191,7 +191,9 @@ export function handleRouteAccess(req, res, next) {
   }
 
   if (role === "TechSecy") {
-    const allowed = [];
+    const allowed = [
+      "/v1/ps"
+    ];
     const startsWithAllowed = [
       "/v1/techsecy/register-team/",
       "/v1/techsecy/get-team/",
@@ -205,8 +207,7 @@ export function handleRouteAccess(req, res, next) {
       "/v1/pssubmission/submit",
       "/v1/pssubmission/view/",
       "/v1/pssubmission/upload-temp",
-      "/v1/ps/protected",
-      "/v1/ps/"
+      "/v1/ps/protected"
     ];
     if (
       !allowed.includes(route) &&
