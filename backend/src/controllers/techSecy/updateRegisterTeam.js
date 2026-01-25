5fba1a342d809ccd8134ce1f8fdcac3728f4cf96
vedantShah30 <pravincj27@gmail.com>
2026-01-25 22:50:04 +0530

    file size updated

--- backend/src/controllers/techSecy/updateRegisterTeam.js
+++ backend/src/controllers/techSecy/updateRegisterTeam.js
@@ -75,7 +75,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 500 * 1024 * 1024 }, // 5MB
   fileFilter,
 }).any();
 
