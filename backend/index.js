a302bde71b095c009f80f3d37f3880216667a9fd
vedantShah30 <pravincj27@gmail.com>
2026-01-26 00:01:47 +0530

    fix: express.json file limit extended

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({limit: '550mb'}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use((req, res, next) => {
